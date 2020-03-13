2952ea66ad6e1862799b014798ab98f8607028bd
vBubbaa <itylerjames@gmail.com>
2020-03-13 13:30:40 -0700

    M2M Serializers created for REST backend

--- djangosteamkit/game/serializers.py
+++ djangosteamkit/game/serializers.py
@@ -1,8 +1,74 @@
 from rest_framework.serializers import ModelSerializer
-from game.models import Game
+from game.models import Game, Price, OSOptions, Languages, AppType, Developer, Publisher, Genre, Category
+
+# M2M and FK relation serializers so we can pull the value and not the ID #
+
+
+class PriceSerializer(ModelSerializer):
+    class Meta:
+        model = Price
+        fields = ['price', 'date']
+
+
+class OSOptionsSerializer(ModelSerializer):
+    class Meta:
+        model = OSOptions
+        fields = ['os']
+
+
+class LanguageSerializer(ModelSerializer):
+    class Meta:
+        model = Languages
+        fields = ['language']
+
+
+class AppTypeSerializer(ModelSerializer):
+    class Meta:
+        model = AppType
+        fields = ['app_type']
+
+
+class DeveloperSerializer(ModelSerializer):
+    class Meta:
+        model = Developer
+        fields = ['developer']
+
+
+class PublisherSerializer(ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = ['publisher']
+
+
+class GenreSerializer(ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['genre_description']
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_description']
+
+
+#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
+
+# Serialize all games in the DB
 
 
 class GameSerializer(ModelSerializer):
+    prices = PriceSerializer(many=True)
+    current_price = PriceSerializer()
+    os = OSOptionsSerializer(many=True)
+    supported_languages = LanguageSerializer(many=True)
+    app_type = AppTypeSerializer()
+    developer = DeveloperSerializer(many=True)
+    publisher = PublisherSerializer(many=True)
+    genres = GenreSerializer(many=True)
+    primary_genre = GenreSerializer()
+    categories = CategorySerializer(many=True)
+
     class Meta:
         model = Game
         fields = '__all__'
