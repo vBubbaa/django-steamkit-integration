ea5db69d03435e98706ef34949634bbb81ccc470
vBubbaa <itylerjames@gmail.com>
2020-03-22 13:54:41 -0700

    Custom paginator for game explore page

--- djangosteamkit/game/pagination.py
+++ djangosteamkit/game/pagination.py
@@ -1,7 +1,18 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework import pagination, response
 
 
-class StandardResultsSetPagination(PageNumberPagination):
+class StandardResultsSetPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'limit'
-    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return response.Response({
+            'pagination': {
+                'total': self.page.paginator.count,
+                'num_pages': self.page.paginator.num_pages,
+                'current_page': self.request.query_params.get('page', None),
+                'next_page_url': self.get_next_link(),
+                'previous_page_url': self.get_previous_link(),
+            },
+            'data': data
+        })
