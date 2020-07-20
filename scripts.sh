9376f6e1a6b64bb5fe6f1269fe4a4f593fd84f47
vBubbaa <itylerjames@gmail.com>
2020-07-20 16:39:25 -0700

    another script change

--- scripts.sh
+++ scripts.sh
@@ -2,5 +2,5 @@
 
 cd /root/django-steamkit-integration
 
-/usr/lib/python3.6 ./manage.py ClientUpdater >> /root/clientlogs.log 2>&1
-/usr/lib/python3.6 ./manage.py TaskUpdater >> /root/tasklogs.log 2>&1
+python3 ./manage.py ClientUpdater
+python3 ./manage.py TaskUpdater
