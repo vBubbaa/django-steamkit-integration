b884bde409c16af23ebdd6977ebbd11e3e2b544b
vBubbaa <itylerjames@gmail.com>
2020-07-22 12:17:50 -0700

    Fix scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -7,6 +7,6 @@ source /root/django-steamkit-integration/scenv/bin/activate
 
 cd /root/django-steamkit-integration
 
-/root/django-steamkit-integration/scenv/bin/python3.7 manage.py ClientUpdater >$
-/root/django-steamkit-integration/scenv/bin/python3.7 manage.py TaskUpdater >> $
+/root/django-steamkit-integration/scenv/bin/python3.7 manage.py ClientUpdater >> /root/clientupdater.log 2>&1
+/root/django-steamkit-integration/scenv/bin/python3.7 manage.py TaskUpdater >> /root/taskupdater.log 2>&1
 
