a7966892e384393239e4ea663d9de34da5ba3ae4
vBubbaa <itylerjames@gmail.com>
2020-09-20 14:45:05 -0700

    get key from settings

--- utils/SteamApiHandler.py
+++ utils/SteamApiHandler.py
@@ -14,7 +14,7 @@ from django.conf import settings
 
 class SteamApi():
     def __init__(self):
-        self.key = '?key=' + str(os.environ.get('STEAM_KEY'))
+        self.key = '?key=' + settings.STEAM_API_KEY
         self.baseurl = 'http://api.steampowered.com'
         self.format = '&format=json'
 
@@ -55,7 +55,7 @@ class SteamApi():
     def getFriendsList(self, steamid):
         method = '/ISteamUser/GetFriendList/v0001/'
         url = settings.STEAM_ROOT_ENDPOINT + method
-        params = {'key': str(os.environ.get('STEAM_KEY')),
+        params = {'key': settings.STEAM_API_KEY,
                   'steamid': steamid, 'relationship': 'friend'}
         request = requests.get(url, params)
         response = request.json()
